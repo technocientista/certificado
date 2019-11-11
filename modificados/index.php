4f4aa8d02627bad823af26c633dbead5554b52f9
technocientista <technocientista@gmail.com>
2019-11-11 17:01:57 -0300

    update

---
+++ modificados/index.php
@@ -0,0 +1,158 @@
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <title></title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  rel="stylesheet">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+  <link rel="stylesheet" type="text/css" href="estilo.css">
+
+</head>
+
+<body>
+
+  <header class="bg-dark">
+
+    <h5 class="bg-dark container-fluid text-white"> Faça login <img src="img/perfil.png" width="4%"> </h5>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        <h5>Sistema Certificado</h5>
+      </a>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+          <li class="nav-item">
+            <a class="nav-link " href="emitir.html"> Certificado</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link " href="suporte.html">Suporte</a>
+          </li><li class="nav-item">
+            <li class="nav-item ">
+              <a class="nav-link " href="login.html">Entrar</a>
+            </li><li class="nav-item">
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+              <button class="btn btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+          </div>
+        </nav>
+
+
+      </header>
+      <div class="container">
+        <div class="row margin-top">
+          <div class="col-xl-12">
+          </div>
+          <div class="col-xl-12">
+            <div class="jumbotron text-center">
+              <h1>
+
+                <h2>O que é o SICERT?</h2>
+                <p class="small">
+                  O SICERT é o sistema de gerenciamento de certificados da UFMS.
+                </p>
+              </div>
+            </div>
+<!--
+            <div class="card border-success col-xs-12 col-md-3" style="max-width: 18rem;">
+              <div class="card-header bg-transparent border-success"><h5 class="card-title">Emissão do Certificado</h5></div>
+              <div class="card-body text-body">
+                
+                <p class="card-text">Clique no botão abaixo se deseja realizar a emissão do certificado.</p>
+              </div>
+              <div class="card-footer bg-transparent border-success"><a href="/emitir-certificados/index" class="btn btn-success" alt="Emitir Certificado" title="Emitir Certificado">Emitir Certificado</a></div>
+            </div>-->
+
+
+            <div class="col-xs-12   col-sm-3">
+              <div class="card card-success">
+                <div class="card-header">
+                  <h5 class="card-title">Emissão do Certificado</h5>
+                </div>
+                <div class="card-body">
+                  <p>Clique no botão abaixo se deseja realizar a emissão do certificado.</p>
+                  
+                    <p class="text-right">
+                    <a href="/emitir-certificados/index" id="btn" class="btn btn-success" alt="Emitir Certificado" title="Emitir Certificado">Emitir Certificado</a>            
+                  </p>
+                  
+                  
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xs-12   col-sm-3">
+              <div class="card card-info">
+                <div class="card-header">
+                  <h5 class="card-title">Validação do Certificado</h5>
+                </div>
+                <div class="card-body">
+                  <p>Clique no botão abaixo se deseja realizar a validação do certificado.</p>
+                  <p class="text-right">
+                    <a href="/validar" class="btn btn-info" alt="Validar Certificado" title="Validar Certificado">Validar Certificado</a>            </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12  col-sm-3">
+                <div class="card card-warning">
+                  <div class="card-header">
+                    <h5 class="card-title">Recuperação de Login/Senha</h5>
+                  </div>
+                  <div class="card-body">
+                    <p>Clique no botão abaixo se deseja realizar a recuperação do seu login/senha.</p>
+                    <p class="text-right">
+                      <a target="_blank" class="btn btn-warning" href="https://passaporte.ufms.br/#/">Recuperar Login/Senha</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12   col-sm-3">
+                <div class="card card-danger">
+                  <div class="card-header">
+                    <h5 class="card-title">Ajuda</h5>
+                  </div>
+                  <div class="card-body">
+                    <p>Clique no botão abaixo se deseja acessar o menu de Ajuda do sistema.</p>
+                    <p class="text-right">
+                      <a href="/ajuda" class="btn btn-danger" alt="Ajuda" title="Ajuda">Ajuda</a>            </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+            <footer>
+              <address>
+                <strong>Sistema de Certificados</strong><br>
+                Av Paulino Felix, 3185, Centro<br>
+                Acopiara, CE<br>
+                Tel: (88) 3565-2751 ou 5083-3884
+              </address>
+              <address>
+                Email: certsis@gmail.com
+              </address>
+            </footer>
+
+
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+          </body>
+
+          </html>
