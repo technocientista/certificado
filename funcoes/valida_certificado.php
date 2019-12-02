2c4e004686deea0df9f48b1272b78bec629576fc
Cicero Magalhães <technocientista@gmail.com>
2019-12-02 16:22:39 -0300

    Atualizado validação #2

--- funcoes/valida_certificado.php
+++ funcoes/valida_certificado.php
@@ -29,5 +29,11 @@ if ($result->num_rows > 0) {
         }
 
     }
+}else{
+    $_SESSION['msg'] = "O código digitado não é válido";
+    $_SESSION['check'] = 1;
+    
+    header("Location: ../validar.php");
+    exit();
 }
 
